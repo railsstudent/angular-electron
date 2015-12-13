fd5b0e3a6b31bd6761dcc32de83253b5eb2b867d
Connie Leung <cleung334@gmail.com>
2015-12-14 00:39:25 +0800

    update controller

--- app/scripts/customer/customerController.js
+++ app/scripts/customer/customerController.js
@@ -1,1 +1,13 @@
-cu
+(function() {
+   'use strict';
+   angular.module('app')
+    .controller('customerController',
+        ['customerService', '$q', '$mdDialog', CustomerController]);
+
+    function CustomerController(customerService, $q, $mdDialog) {
+      var self = this;
+
+      self.selected = null;
+      self.customers = [];
+    };
+})();
