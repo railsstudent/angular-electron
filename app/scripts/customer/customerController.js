8e61417e5201e6ce3c6b20fcf8db1b339b0ee46c
Connie Leung <cleung334@gmail.com>
2015-12-15 01:22:00 +0800

    add controller functions
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- app/scripts/customer/customerController.js
+++ app/scripts/customer/customerController.js
@@ -9,5 +9,28 @@
 
       self.selected = null;
       self.customers = [];
+      self.selectedIndex = 0;
+      self.filterText = null;
+      self.selectCustomer = selectCustomer;
+      self.deleteCustomer = deleteCustomer;
+      self.saveCustomer = saveCustomer;
+      self.createCustomer = createCustomer;
+      self.filter = filterCustomer;
     };
+
+    // load initial customer
+    getAllCustomer();
+
+    //
+    // Internal functions
+    //
+
+    function selectCustomer(customer, index) {
+      self.selected = angular.isNumber(customer) ?
+                          self.customer[customer] : customer;
+      self.selectedIndex = angular.isNumber(customer) ?
+                            customer : index;
+    };
+
+    
 })();
