982bb924760a390b99b9ebb0d8813feb05b83cf2
Connie Leung <cleung334@gmail.com>
2015-12-15 08:15:06 +0800

    Update customer controller and use material design on customer UI.

--- app/scripts/customer/customerController.js
+++ app/scripts/customer/customerController.js
@@ -19,7 +19,7 @@
     };
 
     // load initial customer
-    getAllCustomer();
+    getAllCustomers();
 
     //
     // Internal functions
@@ -32,5 +32,77 @@
                             customer : index;
     };
 
-    
+    function deleteCustomer($event) {
+      var confirm = $mdDialog.confirm()
+                        .title('Are you sure?')
+                        .content('Are you sure want to delete this customer?')
+                        .ok('Yes')
+                        .cancel('No')
+                        .targetEvent($event);
+
+       $mdDialog.show(confirm).then(function() {
+          customerService.destroy(self.selected.customer_id)
+            .then(function(affectedRows) {
+                  self.customers.splice(self.selectedIndex, 1);
+            });
+       }, function() {});
+    };
+
+    function saveCustomer($event) {
+      if (self.selected != null && self.selected.customer_id != null) {
+            customerService.update(self.selected).then(function (affectedRows) {
+                $mdDialog.show(
+                    $mdDialog
+                        .alert()
+                        .clickOutsideToClose(true)
+                        .title('Success')
+                        .content('Data Updated Successfully!')
+                        .ok('Ok')
+                        .targetEvent($event)
+                );
+            });
+        }
+        else {
+            //self.selected.customer_id = new Date().getSeconds();
+            customerService.create(self.selected).then(function (affectedRows) {
+                $mdDialog.show(
+                    $mdDialog
+                        .alert()
+                        .clickOutsideToClose(true)
+                        .title('Success')
+                        .content('Data Added Successfully!')
+                        .ok('Ok')
+                        .targetEvent($event)
+                );
+            });
+        }
+    };
+
+    function createCustomer() {
+      self.selected = {};
+      self.selectedIndex = null;
+    };
+
+    function getAllCustomers() {
+        customerService.getCustomers().then(function(customers) {
+          self.customers = [].concat(customers);
+          self.selected = customers[0];
+        });
+    };
+
+    function filterCustomer() {
+        if (self.filterText == null || self.filterText == "") {
+          getAllCustomers();
+        } else {
+          customerService.getByName(self.filterText).then(function(customers) {
+            self.customers = [].concat(customers);
+            if (customers.length > 0) {
+              self.selected = customers[0]
+            } else {
+              self.selected = null;
+            }
+          });
+        }
+    };
+
 })();
